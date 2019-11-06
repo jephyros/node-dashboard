e8baab81ce6b26edc60c64ce38bc5ef038224e10
cis-WIn <cis@broadwave.co.kr>
2019-11-06 17:23:30 +0900

    Pool.js Error 처리 테스트

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,23 +1,41 @@
 const express = require('express');
 const router = express.Router();
 
-const Mydb = require('../utils/mydb')
+const pool = require('../utils/pool')
+
 
 //const LoginController = 
 
 
 
 router.get('/',(req,res, next)=>{
-	// let mydb = new Mydb(pool);
-	// let users = "cis"
-	let name = ""
+	
+	
+	let resultData ="";
+	let sql = 'select userid,username,inserttime from users where userid=?';
+	let param = ["cis"];	
+	pool(sql,param)
+		.then((result)=>{
+			   console.log("2:",new Date(), result); 
+			   resultData = result[0].username;
+			   res.render('dashboard/dashboard.ejs',{data: resultData})
+			  
+		}).catch(err=>{
+			console.log("err : ", err)
+			resultData = "Error";			
+			res.render('dashboard/dashboard.ejs',{data: resultData})
+			
+		});
+	
+	
+	
 	// mydb.execute(conn => {
 	// 	conn.query("select userid,username from users where userid=?",[users], (err,row )=> {
 	// 		name = row[0].username
 	// 	})
 	// })	
 
-	res.render('dashboard/dashboard.ejs',{data: name})
+	
 })
 
 module.exports = router
--- utils/pool.js
+++ utils/pool.js
@@ -7,66 +7,62 @@ var opts = {
 	port: 3326,
     database:'nodejs'
 };
-function generateQuickGuid() {
-    return Math.random().toString(36).substring(2, 15) +
-        Math.random().toString(36).substring(2, 15);
-}
+
 const pool = mysql.createPool(opts);
-function getRandomInt(min, max){
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-const excuteSql = async(sql,params)=>{
-    try{
-        const user_id = getRandomInt(0, 1000);
-        const access_token = generateQuickGuid();
-        //방법3 
-        try{
-            const connection = await pool.getConnection(async conn=>conn);
-            try{
-                
-                //await connection.beginTransaction();
-                const [rows] = await connection.query(sql,params);
-                //await connection.commit();
-                connection.release();
-                return rows;
-            }catch(err){
-                //await connection.rollback();
-                connection.release();
-                console.log('Query Error!');
-                return false;
-            }
-        }catch(err){
-            console.log('DB ERROR');
-            return false;
-        }
+
+
+
+module.exports =  async function(sql,params) {
+    
+	try{
+		const connection = await pool.getConnection(async conn=>conn);
+		try{
+			
+			//await connection.beginTransaction();
+			const [rows] = await connection.query(sql,params);
+		
+			//await connection.commit();
+			connection.release();
+			return rows;
+		}catch(err){
+			//await connection.rollback();
+			connection.release();
+			console.log('Query Error!');
+			throw new Error("Query Error!");
+			
+		}
+	}catch(err){
+		console.log('DB Connection Error!');
+		throw new Error("DB Connection Error!");
+		
+		
+	}
+	
         
-        
-        
-    }catch(err){
-        console.log('DB ERROR!');
-        return false;
-    }    
 };
  
+
  
-var sql = 'select userid,username,inserttime from users where userid=?';
-var param = ["cis"]
-var sql2 = 'select userid,username,inserttime from users where userid=?';
-var param2 = ["hgd"]
+// var sql = 'select userid,username,inserttime from users where userid=?';
+// var param = ["cis"]
+// var sql2 = 'select userid,username,inserttime from users where userid=?';
+// var param2 = ["hgd"]
 
-var sql3 = 'select userid,username,inserttime from users';
-var param3 = []
+// var sql3 = 'select userid,username,inserttime from users where userid=?';
+// var param3 = ["lss"]
 
-excuteSql(sql,param)
-	.then((res)=>{
-		   console.log("1:", res); 
-		   return excuteSql(sql2,param2)
-	})
-	.then((res)=>{
-		console.log("2:", res); 
-		return excuteSql(sql3,param3)
- 	})
-	.then((res)=>{
-		console.log("3:", res); 
-	});
+// excuteSql(sql,param)
+// 	.then((res)=>{
+// 		   console.log("1:", res); 
+// 		   return excuteSql(sql2,param2)
+// 	})
+// 	.then((res)=>{
+// 		console.log("2:", res); 
+// 		return excuteSql(sql3,param3)
+//  	})
+// 	.then((res)=>{
+// 		console.log("3:", res); 
+// 	}).catch(err=>{
+// 		console.log("err : ", err)
+// 	});
 
