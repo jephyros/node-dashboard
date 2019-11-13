8446d27cb56ae76710e3d1882c0e860a2cdffaf0
cis-WIn <cis@broadwave.co.kr>
2019-11-13 15:53:53 +0900

    logout err log

--- routes/logout.js
+++ routes/logout.js
@@ -6,15 +6,13 @@ const logger = require('../utils/logger');
 
 router.get('/',(req,res, next)=>{
 
-    var session =req.session;
-
-    let username = session.username;
     
-    console.log("sessionname : " ,username);
+    
+    
     req.session.destroy(function(err){
         // cannot access session here
         if(err){
-            logger.info("로그아웃 에러(session.distory")
+            logger.error("로그아웃 에러(session.distory)")        
             //res.redirect('/')
         }
         res.render('index.ejs')
